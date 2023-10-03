79fdddf1ce9416f8547fbdcdc97faa8a1545ab34
Jassi Bacha <klikster@gmail.com>
2023-10-02 19:17:18 -0700

    Snapshots added for colorbox app test

--- 1_colorbox/src/App.test.js
+++ 1_colorbox/src/App.test.js
@@ -4,3 +4,8 @@ import App from './App';
 it('renders without crashing', function () {
     render(<App />);
 });
+
+it('matches snapshot', function () {
+    const { asFragment } = render(<App />);
+    expect(asFragment()).toMatchSnapshot();
+});
